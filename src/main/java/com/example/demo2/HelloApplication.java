7ed94a89ee3b927ae9c39ca4b6d93dd5ebfac3dc
Pasha <pasha.lebedev.2004@gmail.com>
2023-05-04 16:53:49 +0300

    add FXMl file

--- src/main/java/com/example/demo2/HelloApplication.java
+++ src/main/java/com/example/demo2/HelloApplication.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("level1.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 900, 600);
+        stage.setTitle("LEVEL 1");
         stage.setScene(scene);
         stage.show();
     }
