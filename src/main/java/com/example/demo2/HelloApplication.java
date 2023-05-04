5e4abf89dee8a5b5c410d073ebc1565751586990
KingLucky13 <60195892+KingLucky13@users.noreply.github.com>
2023-05-04 16:54:21 +0300

    checkVictory

--- src/main/java/com/example/demo2/HelloApplication.java
+++ src/main/java/com/example/demo2/HelloApplication.java
@@ -6,8 +6,13 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.List;
 
 public class HelloApplication extends Application {
+    Player player=new Player();
+    int[][] field=new int[18][12];
+    List<Enemy> enemies;
+    Bomb[] bombs;
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
@@ -20,12 +25,12 @@ public class HelloApplication extends Application {
     public void checkEnemyTouchPlayer(){}
     public void checkEnemyTouchWalls(){}
     public void checkPlayerTouchWalls(){}
-    public void checkVictory(){}
+    public void checkVictory(){
+        if(enemies.size()==0){
+            System.out.println("win");
+        }
+    }
     public static void main(String[] args) {
-        Player player=new Player();
-        int[][] field=new int[18][12];
-        Enemy[] enemies;
-        Bomb[] bombs;
         launch();
     }
 }
