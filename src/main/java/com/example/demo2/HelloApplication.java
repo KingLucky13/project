33703a4fc24b083a0f55c008d0ed9a82337a0506
Pasha <pasha.lebedev.2004@gmail.com>
2023-05-11 02:56:35 +0300

    create level1

--- src/main/java/com/example/demo2/HelloApplication.java
+++ src/main/java/com/example/demo2/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.demo2;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
