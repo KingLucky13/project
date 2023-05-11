002f0b96e98530921e0e1424991c5e9710c39659
KingLucky13 <60195892+KingLucky13@users.noreply.github.com>
2023-05-11 16:09:22 +0300

    keyListnerForMovement

--- src/main/java/com/example/demo2/HelloApplication.java
+++ src/main/java/com/example/demo2/HelloApplication.java
@@ -1,9 +1,11 @@
 package com.example.demo2;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -27,13 +29,26 @@ public class HelloApplication extends Application {
         stage.getIcons().add(image);
         stage.setScene(level1);
         stage.show();
+        level1.setOnKeyTyped(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                if(keyEvent.getCharacter().equals("w")){
+                    player.move(0,1);
+                }
+                else if (keyEvent.getCharacter().equals("s")) {
+                    player.move(0,-1);
+                }
+                else if (keyEvent.getCharacter().equals("a")) {
+                    player.move(-1,0);
+                }
+                else if (keyEvent.getCharacter().equals("d")) {
+                    player.move(1,0);
+                }
+            }
+        });
     }
 
     public static void main(String[] args) {
-        Player player=new Player();
-        int[][] field=new int[18][12];
-        List<Enemy> enemies = new ArrayList<>();
-        List<Bomb> bombs = new ArrayList<>();
         launch();
     }
 
--- src/main/java/com/example/demo2/Player.java
+++ src/main/java/com/example/demo2/Player.java
@@ -1,5 +1,10 @@
 package com.example.demo2;public class Player {
     int x;
     int y;
-    public void move(int mx,int my){};
+    int speedX;
+    int speedY;
+    public void move(int mx,int my){
+        x+=mx*speedX;
+        y+=mx*speedY;
+    };
 }
