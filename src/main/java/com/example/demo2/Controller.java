8dc0c3f9846190daa4c4b2c850ea8b330d4453ee
Pasha <pasha.lebedev.2004@gmail.com>
2023-05-11 15:44:44 +0300

    first level design

--- src/main/java/com/example/demo2/Controller.java
+++ src/main/java/com/example/demo2/Controller.java
@@ -1,8 +1,11 @@
 package com.example.demo2;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 
 public class Controller {
+    @FXML
+    protected void onMauseClicked(){}
 
 }
--- src/main/java/com/example/demo2/HelloApplication.java
+++ src/main/java/com/example/demo2/HelloApplication.java
@@ -7,26 +7,36 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("level1.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 900, 600);
-        stage.setTitle("LEVEL 1");
-        stage.setScene(scene);
+        FXMLLoader fxmlLoader1 = new FXMLLoader(HelloApplication.class.getResource("homeScreen.fxml"));
+        Scene homeScreen = new Scene(fxmlLoader1.load(), 900, 600);
+        Scene level1 = new Scene(fxmlLoader.load(), 900, 600);
+        stage.setTitle("BOMBASTIC");
+        Image image = new Image("C:\\Users\\m9854\\IdeaProjects\\GUIproject\\src\\main\\resources\\com\\example\\demo2\\bomb_forIcon.png");
+        stage.getIcons().add(image);
+        stage.setScene(level1);
         stage.show();
+    }
+
+    public static void main(String[] args) {
+        Player player=new Player();
+        int[][] field=new int[18][12];
+        List<Enemy> enemies = new ArrayList<>();
+        List<Bomb> bombs = new ArrayList<>();
+        launch();
     }
 
     public void checkEnemyTouchPlayer(){}
     public void checkEnemyTouchWalls(){}
     public void checkPlayerTouchWalls(){}
-    public void checkVictory(){}
-    public static void main(String[] args) {
-        Player player=new Player();
-        int[][] field=new int[18][12];
-        Enemy[] enemies;
-        Bomb[] bombs;
-        launch();
+    public boolean checkVictory(List<Enemy> enemies){
+        return enemies.size() == 0;
     }
 }
